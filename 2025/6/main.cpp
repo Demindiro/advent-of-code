75af0fcf56ce42ba3ef2599f3cdedef9a6b64435
David Hoppenbrouwers <adventofcode@demindiro.com>
2025-12-09 21:37:35 +0100

    day 6 part 2

--- 2025/6/main.cpp
+++ 2025/6/main.cpp
@@ -32,34 +32,85 @@ void read_column_ops(std::vector<char> &out, const std::string &line) {
 
 int main(int argc, char **argv) {
 	(void)argv;
-	(void)argc;
+	bool fuckyou = argc > 1;
 
 	std::vector<std::vector<long long> > nums;
 	std::vector<char> ops;
-	std::string line, next_line;
-	if (!std::getline(std::cin, line)) 
-		abort();
-	while (std::getline(std::cin, next_line)) {
-		nums.push_back({});
-		read_column_numbers(nums.back(), line);
-		std::swap(line, next_line);
-		next_line.clear();
+
+	if (fuckyou) {
+		std::vector<std::string> lines = { "" };
+		while (std::getline(std::cin, lines.back())) 
+			lines.push_back("");
+		lines.pop_back();
+		if (lines.empty())
+			abort();
+		read_column_ops(ops, lines.back());
+		lines.pop_back();
+		size_t cursor = 0;
+		while (cursor < lines[0].size()) {
+			nums.push_back({});
+			std::vector<long long> &v = nums.back();
+			for ( ; cursor < lines[0].size(); ++cursor) {
+				bool all_space = true;
+				long long x = 0;
+				for (const auto &l : lines) {
+					if (l[cursor] == ' ')
+						continue;
+					x = x * 10 + (l[cursor] - '0');
+					all_space = false;
+				}
+				if (all_space)
+					break;
+				v.push_back(x);
+			}
+			++cursor;
+		}
+	} else {
+		std::string line, next_line;
+		if (!std::getline(std::cin, line)) 
+			abort();
+		while (std::getline(std::cin, next_line)) {
+			nums.push_back({});
+			read_column_numbers(nums.back(), line);
+			std::swap(line, next_line);
+			next_line.clear();
+		}
+		read_column_ops(ops, line);
 	}
-	read_column_ops(ops, line);
+
+	for (const auto &v : nums) {
+		for (const auto x : v)
+			std::cout << x << " ";
+		std::cout << std::endl;
+	}
+	for (const auto &x : ops)
+		std::cout << x << " ";
+	std::cout << std::endl;
 
 	long long sum = 0;
 	for (size_t i = 0; i < ops.size(); i++) {
+		std::cout << i << std::endl;
 		long long x;
 		switch (ops[i]) {
 		case '+':
 			x = 0;
-			for (const auto &v : nums)
-				x += v[i];
+			if (fuckyou) {
+				for (const auto v : nums[i])
+					x += v;
+			} else {
+				for (const auto &v : nums)
+					x += v[i];
+			}
 			break;
 		case '*':
 			x = 1;
-			for (const auto &v : nums)
-				x *= v[i];
+			if (fuckyou) {
+				for (const auto v : nums[i])
+					x *= v;
+			} else {
+				for (const auto &v : nums)
+					x *= v[i];
+			}
 			break;
 		default: abort();
 		}

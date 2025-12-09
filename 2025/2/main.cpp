a7323a46867055373def22132fa458ca52705b84
David Hoppenbrouwers <adventofcode@demindiro.com>
2025-12-09 18:17:07 +0100

    day 2 part 2

--- 2025/2/main.cpp
+++ 2025/2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <set>
 
 template<typename T>
 int num_digits(T x) {
@@ -56,11 +57,13 @@ T join(T x, T y) {
 }
 
 int main(int argc, char **argv) {
-	(void)argc;
+	// again: lololol
 	(void)argv;
+	int at_least_twice = argc > 1;
 
 	// in summary:
 	// - sequence repeated *exactly twice*
+	// - (part 2) *at least* twice. Who could have seen this coming?
 
 	std::string line;
 	long long sum = 0;
@@ -75,21 +78,48 @@ int main(int argc, char **argv) {
 		long long lo = atoll(s_lo.c_str()), hi = atoll(s_hi.c_str());
 		std::cout << lo << " - " << hi << std::endl;
 
-		// IDs with odd number of digits are always valid, so skip those
-		int n = num_digits(lo);
-		n += n % 2;
-		for ( ; n <= num_digits(hi); n += 2) {
-			long long lo_m = std::max(cut(lo, n / 2), ipow(10LL, n / 2 - 1)),
-			          hi_m = std::min(cut(hi, n / 2), ipow(10LL, n / 2) - 1);
-			std::cout << ": " << lo_m << " --- " << hi_m << std::endl;
-			for (long long x = lo_m; x <= hi_m; x++) {
-				long long xx = join(x, x);
-				// TODO better calculation of ranges,
-				// but I'm wasting too much time already
-				if (xx < lo || xx > hi)
-					continue;
-				std::cout << "  " << xx << std::endl;
-				sum += xx;
+		if (at_least_twice) {
+			// this is totally dumb but idc
+			// the essential idea: bruteforce!
+			//
+			// This took an embarassingly long amount of time to come up with
+			// as I tried to be too clever and missed a ton of obvious bugs.
+			int n_digits = num_digits(hi);
+			// set to avoid counting duplicates
+			std::set<long long> dedup;
+			// *at least* twice, so / 2
+			for (int n = 1; n <= n_digits / 2; n++) {
+				long long start = ipow(10LL, n - 1), end = ipow(10LL, n);
+				std::cout << "--  " << start << " " << end << std::endl;
+				for (long long x = start; x < end; x++) {
+					long long xx = join(x, x);
+					for (int nn = n * 2; nn <= n_digits; nn += n) {
+						if (xx >= lo && xx <= hi && dedup.find(xx) == dedup.end()) {
+							dedup.insert(xx);
+							std::cout << "  " << xx << std::endl;
+							sum += xx;
+						}
+						xx = join(xx, x);
+					}
+				}
+			}
+		} else {
+			// IDs with odd number of digits are always valid, so skip those
+			int n = num_digits(lo);
+			n += n % 2;
+			for ( ; n <= num_digits(hi); n += 2) {
+				long long lo_m = std::max(cut(lo, n / 2), ipow(10LL, n / 2 - 1)),
+						  hi_m = std::min(cut(hi, n / 2), ipow(10LL, n / 2) - 1);
+				std::cout << ": " << lo_m << " --- " << hi_m << std::endl;
+				for (long long x = lo_m; x <= hi_m; x++) {
+					long long xx = join(x, x);
+					// TODO better calculation of ranges,
+					// but I'm wasting too much time already
+					if (xx < lo || xx > hi)
+						continue;
+					std::cout << "  " << xx << std::endl;
+					sum += xx;
+				}
 			}
 		}
 	}
